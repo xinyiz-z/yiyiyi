7fbb1f3e26f86982114c8107d60ecf280fae47a6
xinyiz-z <2538684584@qq.com>
2026-09-16 16:22:33 +0800

    Delete scrapy directory

--- scrapy/tutorial/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class QuoteItem(scrapy.Item):
-
-    text = scrapy.Field()
-
-    author = scrapy.Field()
-
-    tags = scrapy.Field()
--- scrapy/tutorial/pipelines.py
+++
@@ -1,5 +0,0 @@
-class QuotesPipeline:
-    def process_item(self, item, spider):
-        if item.get('text'):
-            item['text'] = item['text'].replace('“', '').replace('”', '')
-        return item
